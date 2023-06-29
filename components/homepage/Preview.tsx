8b605e6b6973d5e47bb0f3cf55e7c8ecdc06c9a4
Omar Hamid <omarhamid664@gmail.com>
2023-06-29 15:21:33 -0600

    Final video fixes

--- components/homepage/Preview.tsx
+++ components/homepage/Preview.tsx
@@ -7,7 +7,7 @@ const Preview = (): JSX.Element => {
     <div className={styles.menuContainer}>
       <div className={styles.secDiv}>
         <div className={styles.videoContainer}>
-          <iframe src="https://www.youtube.com/embed/hGc_258GUtg?autoplay=1&mute=1&controls=0&disablekb=1&playlist=hGc_258GUtg&loop=1&"></iframe>
+          <iframe src="https://www.youtube.com/embed/hGc_258GUtg?autoplay=1&mute=1&controls=0&disablekb=1&playlist=hGc_258GUtg&loop=1&" />
         </div>
       </div>
     </div>
@@ -15,4 +15,3 @@ const Preview = (): JSX.Element => {
 };
 
 export default Preview;
-// https://www.youtube.com/embed/hGc_258GUtg?autoplay=1&autohide=1&modestbranding=0&rel=0&showinfo=0&controls=0&disablekb=1&enablejsapi=1&iv_load_policy=3&hd=1&mute=1&origin=https%3A%2F%2Fnutrimeals.ca&widgetid=1
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,7 @@ import Preview from "@/components/homepage/Preview";
 export default function Home() {
   return (
     <>
-      <main>
-        <Preview />
-      </main>
+      <Preview />
     </>
   );
 }
