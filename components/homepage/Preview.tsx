25ed175f93f0dcce497976a4e52d3ec9035048e6
Omar Hamid <omarhamid664@gmail.com>
2023-06-29 13:58:12 -0600

    Show stretched video

--- components/homepage/Preview.tsx
+++ components/homepage/Preview.tsx
@@ -4,10 +4,13 @@ import styles from "./Preview.module.css";
 
 const Preview = (): JSX.Element => {
   return (
-    <div className={styles.videoContainer}>
-      <iframe src="https://www.youtube.com/embed/hGc_258GUtg?controls=0&autoplay=1&mute=1&playsinline=1"></iframe>
+    <div className={styles.menuContainer}>
+      <div className={styles.videoContainer}>
+        <iframe src="https://www.youtube.com/embed/hGc_258GUtg?autoplay=1&mute=1&controls=0&disablekb=1&playlist=hGc_258GUtg&loop=1&"></iframe>
+      </div>
     </div>
   );
 };
 
 export default Preview;
+// https://www.youtube.com/embed/hGc_258GUtg?autoplay=1&autohide=1&modestbranding=0&rel=0&showinfo=0&controls=0&disablekb=1&enablejsapi=1&iv_load_policy=3&hd=1&mute=1&origin=https%3A%2F%2Fnutrimeals.ca&widgetid=1
