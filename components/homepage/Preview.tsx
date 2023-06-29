f986e548c20c02ffac098d0f85c51b205c6c79f0
Omar Hamid <omarhamid664@gmail.com>
2023-06-29 01:15:40 -0600

    Make vide full width

--- components/homepage/Preview.tsx
+++ components/homepage/Preview.tsx
@@ -1,8 +1,10 @@
 import { JSX } from "react";
 
+import styles from "./Preview.module.css";
+
 const Preview = (): JSX.Element => {
   return (
-    <div>
+    <div className={styles.videoContainer}>
       <iframe src="https://www.youtube.com/embed/hGc_258GUtg?controls=0&autoplay=1&mute=1&playsinline=1"></iframe>
     </div>
   );
