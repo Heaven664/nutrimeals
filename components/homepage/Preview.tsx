39118f5584f0d24d403863d8a1c7df3cf7a67b24
Omar Hamid <omarhamid664@gmail.com>
2023-06-29 14:45:51 -0600

    Add responsive iframe

--- components/homepage/Preview.tsx
+++ components/homepage/Preview.tsx
@@ -5,8 +5,10 @@ import styles from "./Preview.module.css";
 const Preview = (): JSX.Element => {
   return (
     <div className={styles.menuContainer}>
-      <div className={styles.videoContainer}>
-        <iframe src="https://www.youtube.com/embed/hGc_258GUtg?autoplay=1&mute=1&controls=0&disablekb=1&playlist=hGc_258GUtg&loop=1&"></iframe>
+      <div className={styles.secDiv}>
+        <div className={styles.videoContainer}>
+          <iframe src="https://www.youtube.com/embed/hGc_258GUtg?autoplay=1&mute=1&controls=0&disablekb=1&playlist=hGc_258GUtg&loop=1&"></iframe>
+        </div>
       </div>
     </div>
   );
