9d8f74077d78b6b4195477289f5b500c7967c440
Omar Hamid <omarhamid664@gmail.com>
2023-06-29 19:18:56 -0600

    Add video overlay styling

--- components/homepage/Preview.tsx
+++ components/homepage/Preview.tsx
@@ -1,13 +1,25 @@
 import { JSX } from "react";
+import Link from "next/link";
 
 import styles from "./Preview.module.css";
 
 const Preview = (): JSX.Element => {
   return (
     <div className={styles.menuContainer}>
-      <div className={styles.secDiv}>
-        <div className={styles.videoContainer}>
+      <div className={styles.videoContainer}>
+        <div className={styles.video}>
           <iframe src="https://www.youtube.com/embed/hGc_258GUtg?autoplay=1&mute=1&controls=0&disablekb=1&playlist=hGc_258GUtg&loop=1&" />
+        </div>
+        <div className={styles.videoOverlay}>
+          <h1>Healthy &#38; Ready to Eat Meals Delivered</h1>
+          <h2>
+            The Benefits of Healthy Meal Prep: Save Time and Eat Healthier
+          </h2>
+          <div className={styles.buttonContainer}>
+            <Link href="">
+              <button>View Menu</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
