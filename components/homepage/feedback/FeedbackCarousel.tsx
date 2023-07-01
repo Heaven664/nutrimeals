4200e8a408ca30aded14df05de825faf2638ae50
Omar Hamid <omarhamid664@gmail.com>
2023-07-01 17:40:05 -0600

    Create carousel layout

---
+++ components/homepage/feedback/FeedbackCarousel.tsx
@@ -0,0 +1,24 @@
+import { JSX } from "react";
+
+import styles from "./FeedbackCarousel.module.css";
+
+const FeedbackCarousel = (): JSX.Element => {
+  return (
+    <div className={styles.feedbackLayout}>
+      <div className={styles.feedbackContainer}>
+        <div className={styles.feedbackTitleContainer}>
+          <h3>Healthy meal prep Loved by Locals</h3>
+        </div>
+        <div className={styles.feedbackBodyContainer}>
+          <p>Read what our customers have to say</p>
+        </div>
+        <div className={styles.feedbackCarouser}>Carousel</div>
+        <div className={styles.feedbackButton}>
+          <button>Perfect, I want it</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedbackCarousel;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Preview from "@/components/homepage/preview/Preview";
 import HomepageIntro from "@/components/homepage/intro/HomepageIntro";
+import FeedbackCarousel from "@/components/homepage/feedback/FeedbackCarousel";
 
 export default function Home() {
   return (
     <>
       <Preview />
       <HomepageIntro />
+      <FeedbackCarousel />
     </>
   );
 }
