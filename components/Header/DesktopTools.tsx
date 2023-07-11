d2005ab0aec4ffab2dfa63b95e395feeac9c33a1
Omar Hamid <omarhamid664@gmail.com>
2023-07-11 16:28:09 -0600

    Finish Navbar Desktop tools styling

--- components/Header/DesktopTools.tsx
+++ components/Header/DesktopTools.tsx
@@ -2,40 +2,103 @@ import styles from "./DesktopTools.module.css";
 
 const DesktopTools: React.FC = () => {
   return (
-    <ul>
+    <ul className={styles.toolsList}>
       <li>
-        <div>
+        <div className={styles.toolContainer}>
           <p>Individual Meals</p>
+          <div className={styles.toolExtend}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+            </svg>
+          </div>
         </div>
       </li>
       <li>
-        <div>
+        <div className={styles.toolContainer}>
           <p>Subscription Boxes</p>
+          <div className={styles.toolExtend}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+            </svg>
+          </div>
         </div>
       </li>
       <li>
-        <div>
+        <div className={styles.toolContainer}>
           <p>Meal Plans</p>
+          <div className={styles.toolExtend}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+            </svg>
+          </div>
         </div>
       </li>
       <li>
-        <div>
+        <div className={styles.toolContainer}>
           <p>Bulk Menu</p>
+          <div className={styles.toolExtend}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+            </svg>
+          </div>
         </div>
       </li>
       <li>
-        <div>
+        <div className={styles.toolContainer}>
           <p>Merchandise</p>
+          <div className={styles.toolExtend}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+            </svg>
+          </div>
         </div>
       </li>
       <li>
-        <div>
+        <div className={styles.toolContainer}>
           <p>Gift Card</p>
+          <div className={styles.toolExtend}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+            </svg>
+          </div>
         </div>
       </li>
       <li>
-        <div>
+        <div className={styles.toolContainer}>
           <p>Automated Kiosks</p>
+          <div className={styles.toolExtend}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+            </svg>
+          </div>
         </div>
       </li>
     </ul>
