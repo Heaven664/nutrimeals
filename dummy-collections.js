028d984047232b3435a0bf2652cb00a6f45f4b37
Omar Hamid <omarhamid664@gmail.com>
2023-07-18 09:18:48 -0600

    Create dummy collections

---
+++ dummy-collections.js
@@ -0,0 +1,16 @@
+const DUMMY_COLLECTIONS = [
+  {
+    title: "Autumn Bowl",
+    image: "images/collections/autumn-bowl.webp",
+    price: 16.99,
+  },
+  {
+    title: "Balsamic Maple Pork",
+    image: "images/collections/balsamic-maple-pork.webp",
+    price: 16.99,
+  },
+];
+
+export function getDummyCollections() {
+  return DUMMY_COLLECTIONS;
+}
