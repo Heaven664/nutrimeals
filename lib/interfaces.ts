4859acd55f66c784c44e06e8663e1a634877341c
Omar Hamid <omarhamid664@gmail.com>
2023-07-20 20:42:46 -0600

    Create separate file for interfaces

---
+++ lib/interfaces.ts
@@ -0,0 +1,11 @@
+export interface MealData {
+  id: string;
+  title: string;
+  price: number;
+  image: string;
+  dairyFree: boolean;
+  eggFree: boolean;
+  glutenFree: boolean;
+  peanutFree: boolean;
+  vegetarian: boolean;
+}
--- pages/collections/lunch-and-dinner.tsx
+++ pages/collections/lunch-and-dinner.tsx
@@ -1,19 +1,8 @@
 import CollectionFilter from "@/components/collections/CollectionFilter";
 import CollectionTitle from "@/components/collections/CollectionTitle";
 import CollectionsList from "@/components/collections/CollectionsList";
+import { MealData } from "@/lib/interfaces";
 import { getAllDinnerAndLunchMeals } from "@/lib/mongoDB";
-
-interface MealData {
-  id: string;
-  title: string;
-  price: number;
-  image: string;
-  dairyFree: boolean;
-  eggFree: boolean;
-  glutenFree: boolean;
-  peanutFree: boolean;
-  vegetarian: boolean;
-}
 
 interface P {
   meals: MealData[];
