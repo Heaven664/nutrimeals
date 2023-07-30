707a41ad9437774534d697e780b2519c10ac396c
Omar Hamid <omarhamid664@gmail.com>
2023-07-30 16:49:55 -0700

    Change serveSitePropts to staticProps

--- dummy.js
+++ dummy.js
@@ -1,19 +1,19 @@
 const ong =
 {
   mealData: {
-    title: "BBQ Chicken and Sweet Potato",
-    slug: "balsamic-maple-pork",
+    title: "BBQ Chicken and Sweet Potato Fries",
+    slug: "bbq-chicken-and-sweet-potato-fries",
     price: 16.99,
     imagePath: "/images/collections/bbq-chicken-and-sweet-potato-fries.webp",
-    recipePath: "/images/collections/balsamic-maple-pork-recipe.webp",
+    recipePath: "/images/collections/bbq-chicken-and-sweet-potato-fries-recipe.webp",
     description:
       "Enjoy a delicious herb crusted pork with a side of smashed garlic potatoes and a side of steamed green beans. Comes with a side of sweet and tangy maple balsamic glaze",
     noModifications: true,
     beverageImageContainer: false,
-    date: '2023-11-07T07:00:00.000+00:00'
+    date: '2023-11-06T07:00:00.000+00:00'
   },
   images: [
-    { path: "/images/collections/balsamic-maple-pork.webp", isRecipe: false },
-    { path: "/images/collections/balsamic-maple-pork-recipe.webp", isRecipe: true },
+    { path: "/images/collections/bbq-chicken-and-sweet-potato-fries.webp", isRecipe: false },
+    { path: "/images/collections/bbq-chicken-and-sweet-potato-fries-recipe.webp", isRecipe: true },
   ]
 };
--- pages/products/[...slug].tsx
+++ pages/products/[...slug].tsx
@@ -1,4 +1,4 @@
-import { GetServerSidePropsContext } from "next";
+import { GetStaticPropsContext } from "next";
 import MealProductInfo from "@/components/products/MealProductInfo";
 import SuggestedMeals from "@/components/products/SuggestedMeals";
 import { MealData, ProductData } from "@/lib/interfaces";
@@ -23,10 +23,17 @@ const AutumnBowl = ({ meals, product, error }: P) => {
   );
 };
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async (
+  context: GetStaticPropsContext<{ slug: string[] }>
 ) => {
-  const { slug } = context.query;
+  const { slug } = context.params || { slug: [] };
 
   let meals: MealData[];
   let product: ProductData | null = null;
@@ -36,6 +43,9 @@ export const getServerSideProps = async (
     meals = await getRandomMeals("meals", 4);
     if (Array.isArray(slug)) {
       product = await findProductBySlug("products-meals", slug[0]);
+    }
+    if (!product) {
+      error = true;
     }
   } catch (err) {
     meals = [];
