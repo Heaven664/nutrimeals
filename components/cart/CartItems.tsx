de5dc633eeef2eaa267b4ca719152f9ca14fc359
Omar Hamid <omarhamid664@gmail.com>
2023-09-16 17:04:36 -0600

    Add headers for cart columns

--- components/cart/CartItems.tsx
+++ components/cart/CartItems.tsx
@@ -11,9 +11,26 @@ const CartItems = ({ products }: P) => {
     <div className={styles.CartItemsBody}>
       <div className={styles.cartHeader}>
         <h1>Your cart</h1>
-          <Link href="/">
-            <p>Continue shopping</p>
-          </Link>
+        <Link href="/">
+          <p>Continue shopping</p>
+        </Link>
+      </div>
+      <div className={styles.productsContainer}>
+        <div className={styles.cartItemsHeaders}>
+          <div className={styles.productPreview}>
+            <p>Product</p>
+          </div>
+          <div className={styles.productPrice}>
+            <p>Price</p>
+          </div>
+          <div className={styles.productQuantity}>
+            <p>Quantity</p>
+          </div>
+          <div className={styles.productTotalPrice}>
+            <p>Total</p>
+          </div>
+        </div>
+        <ul></ul>
       </div>
     </div>
   );
