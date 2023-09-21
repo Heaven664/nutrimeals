472011e482faf2baa3593255f36c750e41b100e3
Omar Hamid <omarhamid664@gmail.com>
2023-09-21 14:53:04 -0600

    Add 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
-import EmptyCart from "@/components/cart/EmptyCart";
+import Error404 from "@/components/errors/Error404";
 
 const Custom404 = () => {
-  return <div></div>;
+  return <Error404></Error404>;
 };
 
 export default Custom404;
